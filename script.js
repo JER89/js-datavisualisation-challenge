efb92c12222f8cbc15188d61cdfe7fdbef189867
Laurent-Jazzon <jazzonlaurent@gmail.com>
2021-03-12 09:35:45 +0100

    final

--- script.js
+++ script.js
@@ -17,21 +17,53 @@ canvas3.style.height = "420px"
 let body3 = document.querySelector('#table2 > caption');
 body3.insertBefore(canvas3, body3.childNodes[0]);
 
-// ARRAY CHART 1
-let yData = [];
-getData()
-.then(promise => promise.forEach(element => {
-    console.log(element)
-    yData.push(parseInt(element[1]))
-}))
-console.log(yData)
-
-// FUNCTION AJAX
-async function getData(){
-    let response = await fetch('https://canvasjs.com/services/data/datapoints.php');
-    let data = await response.json()
-    return data;
+// FUNCTION RANDOM RGB 
+function rgb() {
+    let num = Math.round(0xffffff * Math.random());
+    let r = num >> 16;
+    let g = num >> 8 & 255;
+    let b = num & 255;
+    return 'rgb(' + r + ', ' + g +', ' + b +')';
 }
+
+// CHART 1
+let ajaxChart1 = () => {
+    let ctx = document.getElementById("chart1").getContext("2d");
+    let yData = [];
+    let label = [0,1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let i = 10;
+    let chart = new Chart(ctx, {
+    type: "bar",
+    data: {
+        labels: label,
+        datasets: [
+        {
+            label: "Crime statistique",
+            backgroundColor: rgb(),
+            data: yData,
+        },
+        ],
+    },
+    });
+    function chartUp() {
+        i++;
+        label.push(i);
+        fetch("https://canvasjs.com/services/data/datapoints.php",{cache:"no-cache"})
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                yData.length + 1;
+                data.forEach((element) => yData.push(parseInt(element[1])));
+                
+                chart.update();
+                
+            });
+
+        }
+        setInterval(chartUp, 1000);
+    };
+    ajaxChart1();
 
 // DATAS LOCATOR CHART 2
 const table1 = document.getElementById("table1")
@@ -41,6 +73,8 @@ const table1 = document.getElementById("table1")
         let countries = {
             label : table1.rows[z].cells[1].innerHTML,
             data : [],
+            borderColor : rgb(),
+            backgroundColor : 'rgba(0,0,0,0)'
         };
         for(let i =0; i < table1.rows[z].cells.length ; i++){
             if(z === 1 && i > 1){
@@ -61,6 +95,7 @@ let stats2 = [];
         let countries = {
             label : table2.rows[z].cells[1].innerHTML,
             data : [],
+            backgroundColor : rgb(),
         };
         for(let i =0; i < table2.rows[z].cells.length ; i++){
             if(z === 1 && i>1){
@@ -77,13 +112,10 @@ let stats2 = [];
 // CHART 2
 let ctx2 = document.getElementById('chart2').getContext('2d');
 let myChart2 = new Chart(ctx2, {
-    type: 'bar',
+    type: 'line',
     data: {
         labels: labels,
-        datasets: stats,
-            backgroundColor: [
-                rgb()
-            ],
+        datasets: stats
     },
 });
 
@@ -93,27 +125,15 @@ let myChart3 = new Chart(ctx3, {
     type: 'bar',
     data: {
         labels: labels2,
-        datasets: stats2,
-            backgroundColor: [
-                
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+        datasets: stats2
     },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
 });
-// FUNCTION RANDOM RGB 
-
-function rgb() {
-    let num = Math.round(0xffffff * Math.random());
-    let r = num >> 16;
-    let g = num >> 8 & 255;
-    let b = num & 255;
-    return 'rgb(' + r + ', ' + g +', ' + b +')';
-}
